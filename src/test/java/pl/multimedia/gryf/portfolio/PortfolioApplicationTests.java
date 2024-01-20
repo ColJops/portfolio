ed4b3aebda63d7733e95ff38b9714598e88afd50
ColJops <jops24@o2.pl>
2024-01-21 00:54:06 +0100

    Start repo
    Start repo with some files

---
+++ src/test/java/pl/multimedia/gryf/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.multimedia.gryf.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
